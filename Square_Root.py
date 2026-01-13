ad1860ad432e27a4505530df97424e2c47cd2b98
Archana R <archanarkrishna1@gmail.com>
2026-01-13 18:54:46 +0530

    4

---
+++ Square_Root.py
@@ -0,0 +1,13 @@
+# For positive numbers
+num = float(input("Enter a number : "))
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f' %(num ,num_sqrt)) 
+
+# For real or complex numbers
+import cmath
+num = 1 + 2j
+
+num = eval (input('Enter a number : '))
+
+num_sqrt = cmath.sqrt(num)
+print ('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num, num_sqrt.real, num_sqrt.imag))
